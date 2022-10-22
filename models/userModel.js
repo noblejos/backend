9b5ae51f2fcd972ceca720344ce0d10e067a53cf
noblejos <98827185+noblejos@users.noreply.github.com>
2022-10-23 00:53:49 +0100

    cleanup

--- models/userModel.js
+++ models/userModel.js
@@ -16,15 +16,4 @@ const userSchema = new Schema({
     }
 })
 
-// userSchema.statics.signup = async function(email, password){
-// const exists= await this.findOne({email})
-// if (exists){
-//     throw Error('Email already in use')
-// }
-// const salt = await bcrypt.genSalt(10)
-// const hash = await bcrypt.hash(password, salt)
-
-// const user = await this.create({email, password:hash})
-// return user
-// }
 module.exports=mongoose.model('User', userSchema)
--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,24 +1,5 @@
 const mongoose = require("mongoose")
 
-// const Schema= mongoose.Schema
-
-
-// const workSchema = {
-//     workout:new mongoose.Schema({
-//         title:{
-//             type: String,
-//             required: true
-//         },
-//         reps:{
-//             type: Number,
-//             required: true
-//         },
-//         load:{
-//             type:Number,
-//             requried:true
-//         }
-// },{timestamps: true}),
-// }
 
 
 const  workoutSchema = new mongoose.Schema({
